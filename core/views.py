98a26ee6cd6496760c9cde2799d39ea562e85c88
M H LIKHON <115473540+MH-LIKHON@users.noreply.github.com>
2025-05-28 10:45:48 +0000

    Connected home view to home.html template and fixed static tag loading

--- core/views.py
+++ core/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
-
-
-
-from django.http import HttpResponse
-
-# Created a simple view here just to test if the project setup is working.
-# This should return a plain message at the homepage URL.
+# This view now renders the homepage using the base layout and home.html
 def home(request):
-    return HttpResponse("Welcome to VelVady!")
+    return render(request, 'core/home.html')
