ec6bb1cc2e6629a0d5867b65936466f7f4714c90
M H LIKHON <115473540+MH-LIKHON@users.noreply.github.com>
2025-06-09 22:10:26 +0000

    Switch to Cloudinary URLs using URLField, fix template image rendering

--- products/models.py
+++ products/models.py
@@ -17,7 +17,7 @@ class Service(models.Model):
     slug = models.SlugField(max_length=200, unique=True, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image = models.ImageField(upload_to='products/')
+    image = models.URLField(max_length=500, null=True, blank=True)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
