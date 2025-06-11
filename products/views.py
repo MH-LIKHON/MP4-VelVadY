5eb30cfd9d3e1b00e274e4b326997c9c97da1e47
M H LIKHON <115473540+MH-LIKHON@users.noreply.github.com>
2025-06-11 00:56:24 +0000

    Fix Stripe URLs by adding https scheme

--- products/views.py
+++ products/views.py
@@ -117,7 +117,7 @@ def create_checkout_session(request, service_id):
         stripe.api_key = settings.STRIPE_SECRET_KEY
         service = Service.objects.get(id=service_id)
 
-        domain_url = DOMAIN
+        domain = os.getenv("DOMAIN", "https://velvady-app-b7f67234cb3b.herokuapp.com")
 
         session = stripe.checkout.Session.create(
             payment_method_types=["card"],
